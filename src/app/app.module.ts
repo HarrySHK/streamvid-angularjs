de9e7b2717e9b717b4b8385998e5e312e1ad7c07
M Haris Nadeem <harisnadeemshk@gmail.com>
2023-10-11 03:14:13 +0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './My_Components/navbar/navbar.component';
+import { SliderComponent } from './My_Components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
